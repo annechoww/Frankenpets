da0f0c004bf8eea99c82377e304df9f9d99a44ba
Chelsea Wang <chelseawang9@gmail.com>
2025-02-14 14:35:25 -0500

    movement script bug fix

--- Frankenpets/Assets/Scripts/PetMovement.cs
+++ Frankenpets/Assets/Scripts/PetMovement.cs
@@ -3,137 +3,132 @@ using UnityEngine;
 public class PetMovement : MonoBehaviour
 {
     public float walkSpeed = 0.8f;
-    public float turnSpeed = 0.5f;
-    public float turnBoost = 0.5f;
+    public float frontTurnSpeed = 1.5f;
+    public float backTurnSpeed = 0.7f;
+
+    // public float turnBoost = 0.5f;
     public Transform frontHalf;
     public Transform backHalf;
-    private FixedJoint fixedJoint;
+    // private FixedJoint fixedJoint;
 
     void Start()
     {
-        fixedJoint = frontHalf.GetComponent<FixedJoint>();
+        // fixedJoint = frontHalf.GetComponent<FixedJoint>();
     }
 
     void Update()
     {
-        fixedJoint = frontHalf.GetComponent<FixedJoint>();
+        // fixedJoint = frontHalf.GetComponent<FixedJoint>();
+        setFrontHalfMovement();
+        setBackHalfMovement();
 
-        if (fixedJoint != null) 
-        {
-            Vector3 frontHalfDirection = getFrontDirection(isSplit: false);
-            Vector3 backHalfDirection = getBackDirection(isSplit: false);
-            setConnectedMovement(frontHalfDirection, backHalfDirection);
-        }
-        else 
-        {
-            Vector3 frontHalfDirection = getFrontDirection(isSplit: true);
-            Vector3 backHalfDirection = getBackDirection(isSplit: true);
-            setSplitMovement(frontHalfDirection, backHalfDirection);
-        }
-
-        // setInPlace();
     }
 
-    void setSplitMovement(Vector3 frontHalfDirection, Vector3 backHalfDirection)
+    // TODO: turn into one function with parameters for controls (because plays can switch halves)
+    void setFrontHalfMovement() 
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) frontHalf.Rotate(1.0f, 0.0f, 0.0f, Space.Self);
-        if (Input.GetKey(KeyCode.RightArrow)) frontHalf.Rotate(-1.0f, 0.0f, 0.0f, Space.Self);
-
-        if (Input.GetKey(KeyCode.A)) backHalf.Rotate(0.0f, 1.0f, 0.0f, Space.Self);
-        if (Input.GetKey(KeyCode.D)) backHalf.Rotate(0.0f, -1.0f, 0.0f, Space.Self);
-
-        frontHalf.Translate(frontHalfDirection * Time.deltaTime, Space.Self);
-        backHalf.Translate(backHalfDirection * Time.deltaTime, Space.Self);
+        if (Input.GetKey(KeyCode.LeftArrow)) frontHalf.Rotate(0.0f, -frontTurnSpeed, 0.0f, Space.Self);
+        if (Input.GetKey(KeyCode.RightArrow)) frontHalf.Rotate(0.0f, frontTurnSpeed, 0.0f, Space.Self);
+        if (Input.GetKey(KeyCode.UpArrow)) frontHalf.Translate(Vector3.forward * walkSpeed * Time.deltaTime, Space.Self);
+        if (Input.GetKey(KeyCode.DownArrow)) frontHalf.Translate(Vector3.back * walkSpeed * Time.deltaTime, Space.Self);
+    }
+    void setBackHalfMovement() 
+    {
+        if (Input.GetKey(KeyCode.A)) backHalf.Rotate(0.0f, -backTurnSpeed, 0.0f, Space.Self);
+        if (Input.GetKey(KeyCode.D)) backHalf.Rotate(0.0f, backTurnSpeed, 0.0f, Space.Self);
+        if (Input.GetKey(KeyCode.W)) backHalf.Translate(Vector3.forward * walkSpeed * Time.deltaTime, Space.Self);
+        if (Input.GetKey(KeyCode.S)) backHalf.Translate(Vector3.back * walkSpeed * Time.deltaTime, Space.Self);
     }
 
-    void setConnectedMovement(Vector3 frontHalfDirection, Vector3 backHalfDirection)
-    {
-        // Check if both halves are trying to turn together while connected
-        if (bothHalvesTurningLeft()) 
-        {
-            // Faster turn speed if both halves are turning together
-            backHalfDirection = new Vector3(0, backHalfDirection.y, backHalfDirection.z);
-            frontHalfDirection += Vector3.left * turnBoost; 
-        }
+    // void setSplitMovement(Vector3 frontHalfDirection, Vector3 backHalfDirection)
+    // {
+    //     if (Input.GetKey(KeyCode.LeftArrow)) frontHalf.Rotate(0.0f, 1.0f, 0.0f, Space.Self);
+    //     if (Input.GetKey(KeyCode.RightArrow)) frontHalf.Rotate(0.0f, -1.0f, 0.0f, Space.Self);
 
-        if (bothHalvesTurningRight()) 
-        {
-            backHalfDirection = new Vector3(0, backHalfDirection.y, backHalfDirection.z);
-            frontHalfDirection += Vector3.right * turnBoost; 
-        }
+    //     if (Input.GetKey(KeyCode.A)) backHalf.Rotate(0.0f, 1.0f, 0.0f, Space.Self);
+    //     if (Input.GetKey(KeyCode.D)) backHalf.Rotate(0.0f, -1.0f, 0.0f, Space.Self);
+        
 
-        // if (bothHalvesTurningOpposite())
-        // {
+    //     frontHalf.Translate(frontHalfDirection * Time.deltaTime, Space.Self);
+    //     backHalf.Translate(backHalfDirection * Time.deltaTime, Space.Self);
+    // }
+
+    // void setConnectedMovement(Vector3 frontHalfDirection, Vector3 backHalfDirection)
+    // {
+    //     // Check if both halves are trying to turn together while connected
+    //     if (bothHalvesTurningLeft()) 
+    //     {
+    //         // Faster turn speed if both halves are turning together
+    //         backHalfDirection = new Vector3(0, backHalfDirection.y, backHalfDirection.z);
+    //         frontHalfDirection += Vector3.left * turnBoost; 
+    //     }
+
+    //     if (bothHalvesTurningRight()) 
+    //     {
+    //     }
+    //     if (bothHalvesTurningRight()) 
+    //     {
+    //         backHalfDirection = new Vector3(0, backHalfDirection.y, backHalfDirection.z);
+    //         frontHalfDirection += Vector3.right * turnBoost; 
+    //     }
+
+    //     // if (bothHalvesTurningOpposite())
+    //     // {
             
-        // }
+    //     // }
 
-        frontHalf.Translate(frontHalfDirection * Time.deltaTime, Space.Self);
-        backHalf.Translate(backHalfDirection * Time.deltaTime, Space.Self);
-    }
+    //     frontHalf.Translate(frontHalfDirection * Time.deltaTime, Space.Self);
+    //     backHalf.Translate(backHalfDirection * Time.deltaTime, Space.Self);
+    // }
 
     // Set the direction vector for the front half. 
     // Do not use Time.deltaTime here; it's used in setMovement().
-    Vector3 getFrontDirection(bool isSplit)
-    {
-        Vector3 direction = new Vector3();
+    // private Vector3 getFrontDirection(bool isSplit)
+    // {
+    //     Vector3 direction = new Vector3();
 
-        if (Input.GetKey(KeyCode.LeftArrow) && !isSplit) direction += Vector3.left * turnSpeed;
-        if (Input.GetKey(KeyCode.RightArrow) && !isSplit) direction += Vector3.right * turnSpeed;
-        if (Input.GetKey(KeyCode.UpArrow)) direction += Vector3.forward * walkSpeed;
-        if (Input.GetKey(KeyCode.DownArrow)) direction += Vector3.back * walkSpeed;
+    //     if (Input.GetKey(KeyCode.LeftArrow) && !isSplit) direction += Vector3.left * turnSpeed;
+    //     if (Input.GetKey(KeyCode.RightArrow) && !isSplit) direction += Vector3.right * turnSpeed;
+    //     if (Input.GetKey(KeyCode.UpArrow)) direction += Vector3.forward * walkSpeed;
+    //     if (Input.GetKey(KeyCode.DownArrow)) direction += Vector3.back * walkSpeed;
 
-        return direction;
-    }
+    //     return direction;
+    // }
 
-    Vector3 getBackDirection(bool isSplit)
-    {
-        Vector3 direction = new Vector3();
+    // private Vector3 getBackDirection(bool isSplit)
+    // {
+    //     Vector3 direction = new Vector3();
 
-        if (Input.GetKey(KeyCode.A) && !isSplit) direction += Vector3.left * turnSpeed;
-        if (Input.GetKey(KeyCode.D) && !isSplit) direction += Vector3.right * turnSpeed;
-        if (Input.GetKey(KeyCode.W)) direction += Vector3.forward * walkSpeed;
-        if (Input.GetKey(KeyCode.S)) direction += Vector3.back * walkSpeed;
+    //     if (Input.GetKey(KeyCode.A) && !isSplit) direction += Vector3.left * turnSpeed;
+    //     if (Input.GetKey(KeyCode.D) && !isSplit) direction += Vector3.right * turnSpeed;
+    //     if (Input.GetKey(KeyCode.W)) direction += Vector3.forward * walkSpeed;
+    //     if (Input.GetKey(KeyCode.S)) direction += Vector3.back * walkSpeed;
 
-        return direction;
-    }
+    //     return direction;
+    // }
 
-    bool bothHalvesTurningLeft()
-    {
-        if (fixedJoint != null && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.A)) return true;
+    // private bool bothHalvesTurningLeft()
+    // {
+    //     if (fixedJoint != null && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.A)) return true;
+    //     if (fixedJoint != null && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.A)) return true;
         
-        return false;
-    }
+    //     return false;
+    // }
 
-    bool bothHalvesTurningRight()
-    {
-        if (fixedJoint != null && Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.D)) return true;
+    // private bool bothHalvesTurningRight()
+    // {
+    //     if (fixedJoint != null && Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.D)) return true;
         
-        return false;
-    }
+    //     return false;
+    // }
 
-    bool bothHalvesTurningOpposite()
-    {
-        if (fixedJoint != null && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.D)) return true;
+    // private bool bothHalvesTurningOpposite()
+    // {
+    //     if (fixedJoint != null && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.D)) return true;
 
-        if (fixedJoint != null && Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.A)) return true;
+    //     if (fixedJoint != null && Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.A)) return true;
 
-        return false;
-    }
+    //     return false;
+    // }
 
-    // temporary method for developing the backside's swinging motion
-    // Plant the front half's body in place (mimicking the cat's climbing function)
-    void setInPlace()
-    {
-        if (Input.GetKey(KeyCode.F)) 
-        {
-            frontHalf.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            Debug.Log("Front half constraints are frozen.");
-        }
-
-        if (Input.GetKey(KeyCode.G)) 
-        {
-            frontHalf.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            Debug.Log("Unfrozen.");
-        }
-    }
 }
