bed4be91ea183286e0cc9bd75d30dbe38d6dd99d
Chelsea Wang <chelseawang9@gmail.com>
2025-02-21 09:08:52 -0500

    minor changes to grab script + scene lighting and tag fixes

--- Frankenpets/Assets/Scripts/GrabScript.cs
+++ Frankenpets/Assets/Scripts/GrabScript.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class GrabScript : MonoBehaviour
 {
-    public float maxHitDistance = 50;
+    // public float maxHitDistance = 50;
     private RaycastHit hit;
     private Collider grabbableObject;
     private bool canGrab = false;
     private bool isGrabbing = false;
     private GameObject grabText;
+    private Vector3 mouthPosition;
+    private Vector3 mouthDirection;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,8 +22,8 @@ public class GrabScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // FOR DEBUGGING
-        // Vector3 mouthPosition = transform.position + transform.TransformDirection(Vector3.forward * 0.23f + Vector3.up * 0.202f + Vector3.right * 0.2f); //0.24f
+        // FOR DEBUGGING: Make sure mouthPosition and mouthDirection match the one at line 54, and comment out the variables on lines 54 & 55
+        // Vector3 mouthPosition = transform.position + transform.TransformDirection(Vector3.forward * 0.23f + Vector3.up * 0.202f);
         // Vector3 mouthDirection = mouthPosition + transform.TransformDirection(Vector3.forward); //Vector3.forward * 0.34f + 
         // Debug.DrawLine(mouthPosition, Vector3.forward + Vector3.up, Color.red, 2, false);
         // Debug.DrawLine(mouthPosition, mouthDirection, Color.red, 2, false);
@@ -47,11 +51,8 @@ public class GrabScript : MonoBehaviour
             // grabbableObjectJoint.connectedBody = transform.GetChild(0).transform.GetComponent<Rigidbody>();
             ////////////////////////////////////////////////////////
             
-            Vector3 mouthPosition = transform.position + transform.TransformDirection(Vector3.forward * 0.34f + Vector3.up * 0.3f + Vector3.right * 0.2f); //0.24f
-            Vector3 mouthDirection = mouthPosition + transform.TransformDirection(Vector3.forward);
-
-            // Debug.DrawLine(mouthPosition, Vector3.forward + Vector3.up, Color.red, 2, false);
-            // Debug.DrawLine(mouthPosition, mouthDirection, Color.red, 2, false);
+            mouthPosition = transform.position + transform.TransformDirection(Vector3.forward * 0.23f + Vector3.up * 0.202f);
+            // mouthDirection = mouthPosition + transform.TransformDirection(Vector3.forward);
 
             // Future: maybe need to Raycast (double hit) to calculate mouth opening angle 
             // if (Physics.Raycast(mouthPosition, grabbableObject.transform.position, out hit, maxHitDistance))
