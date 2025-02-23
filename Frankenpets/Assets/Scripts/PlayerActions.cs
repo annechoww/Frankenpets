237660130d4b2faa25b1f6e02b03639ad20c56ff
Anne Chow <anne.chow123@hotmail.com>
2025-02-23 02:12:29 -0500

    Migrated climbing and grab.

--- Frankenpets/Assets/Scripts/PlayerActions.cs
+++ Frankenpets/Assets/Scripts/PlayerActions.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
 
 /**
-This class handles all actions that do not require collision detection 
-    - Jump
-    - Make Noise
-    - Stand on Hind Legs
-    - Charged Jump
-
-Actions NOT implemented in this script:
-    - Climb
-    - Grab
-    - Tail Usage
-    - Front Paw Usage
+This Class handles all player actions. The actions are mapped as follows:
+    - (C) and (/) handles the special actions of the corresponding half and its species (Climb, Grab, Charged Jump, Hind Legs[TBF])
+    ----------------------------------------------
+    |         Front         |        Back        |
+    |--------------------------------------------|
+    | Make Noise (X or .)   | Jump (X or ,)      |
+    | Front Paws (Z or ,)   | Use Tail (Z or ,)  |
+    ----------------------------------------------
 */
+
 public class PlayerActions : MonoBehaviour
 {
     [Header("Jumping Variables")]
@@ -26,6 +24,23 @@ public class PlayerActions : MonoBehaviour
     public AudioClip dogClip;
     public AudioClip catClip;
 
+    [Header("Climbing Settings")]
+    public float climbSpeed = 2f;
+    public float climbCheckDistance = 0.5f;
+    private bool isNearClimbable = false;
+    private bool isClimbing = false;
+    private Vector3 climbDirection;
+    private GameObject climbText;
+
+    // Grabbing Variables
+    private RaycastHit hit;
+    private Collider grabbableObject;
+    private bool canGrab = false;
+    private bool isGrabbing = false;
+    private GameObject grabText;
+    private Vector3 mouthPosition;
+    private Vector3 mouthDirection;
+
     private PlayerManager playerManager; 
     private Player P1;
     private Player P2;
@@ -35,6 +50,82 @@ public class PlayerActions : MonoBehaviour
     private Rigidbody backRb;
 
     private void Start()
+    {
+        climbText = GameObject.FindGameObjectWithTag("ClimbText");
+        grabText = GameObject.FindGameObjectWithTag("GrabText");
+        
+        Invoke("getPlayerManager", 1f);
+    }
+
+    private void Update()
+    {
+        runJumpLogic();
+        runNoiseLogic();
+        runClimbLogic();
+        runGrablogic();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            // Apply climbing movement in FixedUpdate for smooth physics
+            frontRb.linearVelocity = climbDirection * climbSpeed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            // Show UI Popover
+            climbText.SetActive(true);
+            climbText.transform.position = other.transform.position + (Vector3.forward * 0.05f);
+
+            isNearClimbable = true;
+            // Get the surface normal to determine climb direction
+            if (Physics.Raycast(transform.position, other.transform.position - transform.position, out RaycastHit hit, climbCheckDistance))
+            {
+                // Project the up vector onto the surface plane to get the climb direction
+                climbDirection = Vector3.ProjectOnPlane(Vector3.up, hit.normal).normalized;
+            }
+            else
+            {
+                climbDirection = Vector3.up;
+            }
+        } else if (other.CompareTag("Grabbable") && !isGrabbing)
+        {
+            grabbableObject = other;
+            
+            // Show UI Popover
+            grabText.transform.position = grabbableObject.transform.position + (Vector3.up * 0.30f);
+            grabText.SetActive(true);
+            
+            canGrab = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            // Hide UI popover
+            climbText.SetActive(false);
+
+            isNearClimbable = false;
+            if (isClimbing)
+            {
+                StopClimbing();
+            }
+        } else if (other.CompareTag("Grabbable"))
+        {
+            // Hide UI Popover
+            grabText.SetActive(false);
+            canGrab = false;
+        }
+    }
+
+    private void getPlayerManager()
     {
         // Find the PlayerManager in the scene
         playerManager = FindObjectOfType<PlayerManager>();
@@ -58,26 +149,18 @@ public class PlayerActions : MonoBehaviour
         {
             Debug.Log("Error in fetching playerManager");
         }
-
     }
-
-    private void Update()
-    {
-        runJumpLogic();
-        runNoiseLogic();
-    }
-
 
 ////////////////////////////////////// Jump Logic /////////////////////////////////////
     private void runJumpLogic()
     {
         // Basic Jump
-        if ((Input.GetKey(KeyCode.Z) && !P1.IsFront) || ((Input.GetKey(KeyCode.M)) && !P2.IsFront))
+        if ((Input.GetKey(KeyCode.Z) && !P1.IsFront) || ((Input.GetKey(KeyCode.Comma)) && !P2.IsFront))
         {
             tryStartJump(jumpForce, jumpCooldown);
         }
-        // Charged Jump
-        else if ((Input.GetKey(KeyCode.V) && !P1.IsFront && P1.Species == "cat") || 
+        // Charged Jump - TODO: Update with valid species 
+        else if ((Input.GetKey(KeyCode.C) && !P1.IsFront && P1.Species == "cat") || 
                 (Input.GetKey(KeyCode.Slash) && !P2.IsFront && P2.Species == "dog"))
         {
             tryStartJump(chargedJumpForce, chargedJumpCooldown);
@@ -97,7 +180,7 @@ public class PlayerActions : MonoBehaviour
 ////////////////////////////////////// Noise Logic /////////////////////////////////////
     private void runNoiseLogic()
     {
-        if ((Input.GetKey(KeyCode.X) && P1.IsFront) || ((Input.GetKey(KeyCode.Comma)) && P2.IsFront))
+        if ((Input.GetKey(KeyCode.X) && P1.IsFront) || ((Input.GetKey(KeyCode.Period)) && P2.IsFront))
         {
             // TODO: Add animation trigger when this happens to frontHalf
             string frontSpecies = P1.IsFront ? P1.Species : P2.Species;
@@ -112,5 +195,71 @@ public class PlayerActions : MonoBehaviour
         }
     }
 
+////////////////////////////////////////// Climb Action ///////////////////////////////////////////////
+    private void runClimbLogic()
+    {
+        // TODO: ADD CHECK FOR CAT
+        if (isNearClimbable && !isClimbing)
+        {
+            if ((Input.GetKeyDown(KeyCode.C) && P1.IsFront) || (Input.GetKeyDown(KeyCode.Slash) && P2.IsFront))
+            {
+                StartClimbing();
+            }
+        }
+        else if (((Input.GetKeyUp(KeyCode.C) && P1.IsFront) || (Input.GetKeyUp(KeyCode.Slash) && P2.IsFront)) && isClimbing)
+        {
+            StopClimbing();
+        }
+    }
 
+    private void StartClimbing()
+    {
+        
+        isClimbing = true;
+        frontRb.useGravity = false;
+        // Zero out current velocities
+        frontRb.linearVelocity = Vector3.zero;
+        frontRb.angularVelocity = Vector3.zero;
+        // Freeze rotation while climbing
+        frontRb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void StopClimbing()
+    {
+        isClimbing = false;
+        frontRb.useGravity = true;
+        frontRb.constraints = RigidbodyConstraints.None;
+    }
+
+////////////////////////////////////////// Grab Action ///////////////////////////////////////////////
+    private void runGrablogic() // TODO: Add dog species check
+    {
+        if (((Input.GetKeyDown(KeyCode.C) && P1.IsFront) || (Input.GetKeyDown(KeyCode.Slash) && P2.IsFront)) && isGrabbing)
+        {
+            Debug.Log("Released item");
+            grabbableObject.transform.SetParent(null);
+            
+            grabbableObject.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            Physics.IgnoreLayerCollision(10, 9, false);
+
+            isGrabbing = false;
+
+        } else if (((Input.GetKeyDown(KeyCode.C) && P1.IsFront) || (Input.GetKeyDown(KeyCode.Slash) && P2.IsFront)) && canGrab)
+        {
+            Debug.Log("Grabbed item");
+            
+            mouthPosition = transform.position + transform.TransformDirection(Vector3.forward * 0.23f + Vector3.up * 0.202f);
+            grabbableObject.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            Physics.IgnoreLayerCollision(10, 9, true);
+
+            grabbableObject.transform.SetParent(transform);
+            grabbableObject.transform.position = mouthPosition;
+
+            isGrabbing = true;
+            grabText.SetActive(false);
+        }
+    }
+
+////////////////////////////////////////// Hind Legs Action ///////////////////////////////////////////////
+// TODO: Fix hind legs prior to adding to script here.
 }
