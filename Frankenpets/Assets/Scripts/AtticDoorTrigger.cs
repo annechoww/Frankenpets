4f463b7ef9d79571598a5c42dc85082031359daf
Chelsea Wang <chelseawang9@gmail.com>
2025-02-25 12:42:21 -0500

    anotha fix

--- Frankenpets/Assets/Scripts/AtticDoorTrigger.cs
+++ Frankenpets/Assets/Scripts/AtticDoorTrigger.cs
@@ -6,7 +6,7 @@ public class AtticDoorTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("cat front"))
+        if (other.CompareTag("dog front"))
         {
             if (levelLoader != null) 
             {
