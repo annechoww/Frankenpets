75036946f4d8adc2c9e64274d8267909d6008fc1
Anne Chow <anne.chow123@hotmail.com>
2025-03-11 08:19:52 -0400

    Fixed attic prevention collider

--- Frankenpets/Assets/Scripts/AtticTasks/AtticPrevention.cs
+++ Frankenpets/Assets/Scripts/AtticTasks/AtticPrevention.cs
@@ -14,13 +14,14 @@ public class AtticPrevention : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(tutorialText.getCurrTutorialStage());
         if (other.CompareTag("dog front") && (tutorialText.getCurrTutorialStage() == TutorialText.tutComplete || tutorialText.getCurrTutorialStage() == TutorialText.annoyedCat))
         {
             UnityEngine.Debug.Log("attic prevention");
             blocker.SetActive(true);
             tutorialText.leaveAtticSpeech();
             
-        } else if (other.CompareTag("cat front") && (tutorialText.getCurrTutorialStage() == TutorialText.scaredDog || tutorialText.getCurrTutorialStage() == TutorialText.annoyedCat))
+        } else if (other.CompareTag("cat front") && (tutorialText.getCurrTutorialStage() == TutorialText.scaredDog || tutorialText.getCurrTutorialStage() == TutorialText.leaveAttic))
         {
             UnityEngine.Debug.Log("can fall ");
             blocker.SetActive(false);
