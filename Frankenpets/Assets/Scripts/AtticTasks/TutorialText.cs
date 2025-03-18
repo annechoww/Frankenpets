d972affe851da93810f610705531fb4653a6d583
Chelsea Wang <chelseawang9@gmail.com>
2025-03-17 22:41:34 -0400

    fixed controls corner for tutorial

--- Frankenpets/Assets/Scripts/AtticTasks/TutorialText.cs
+++ Frankenpets/Assets/Scripts/AtticTasks/TutorialText.cs
@@ -172,6 +172,9 @@ public class TutorialText : MonoBehaviour
         // P2ControlsCB = controlsCornerUIChild.transform.GetChild(1).transform.GetChild(2).gameObject;
         P2ControlsDB = controlsCornerUIChild.transform.GetChild(1).transform.GetChild(1).gameObject; // old index was 3
 
+        P2ControlsGrab.transform.GetChild(0).gameObject.SetActive(isKeyboard);
+        P2ControlsGrab.transform.GetChild(1).gameObject.SetActive(isKeyboard);
+
         if (isKeyboard){
             overlay.transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
         } else if (!isKeyboard){
