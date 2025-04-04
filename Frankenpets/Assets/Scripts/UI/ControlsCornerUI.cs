e52745a821f3b76fdcbcc3393066619ffdf26366
rohanbrohan <rohanbrohan@gmail.com>
2025-04-04 12:22:00 -0400

    added button highlights to mini controls UI

--- Frankenpets/Assets/Scripts/UI/ControlsCornerUI.cs
+++ Frankenpets/Assets/Scripts/UI/ControlsCornerUI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ControlsCornerUI : MonoBehaviour
 {
@@ -14,6 +15,47 @@ public class ControlsCornerUI : MonoBehaviour
     [Header("Script references")]
     private ControllerAssignment controllerAssignment;
     public PlayerManager playerManager;
+
+    [Header("P1 Front Control Icons")]
+    public Image p1SwitchIcon;
+    public Image p1ReconnectIcon;
+    public Image p1FrontPawIcon;
+    public Image p1MeowIcon;
+    public Image p1ClimbIcon;
+
+    [Header("P2 Front Control Icons")]
+    public Image p2SwitchIcon;
+    public Image p2ReconnectIcon;
+    public Image p2FrontPawIcon;
+    public Image p2BarkIcon;
+    public Image p2GrabIcon;
+
+    [Header("P1 Back Control Icons")]
+    public Image p1BackSwitchIcon;
+    public Image p1BackReconnectIcon;
+    public Image p1JumpIcon;
+    public Image p1TailIcon;
+    public Image p1HighJumpIcon;
+    
+    [Header("P2 Back Control Icons")]
+    public Image p2BackSwitchIcon;
+    public Image p2BackReconnectIcon;
+    public Image p2JumpIcon;
+    public Image p2TailIcon;
+    public Image p2DashIcon;
+
+    [Header("Icon States")]
+    public Sprite RTNormal;
+    public Sprite RTPressed;
+    public Sprite LTNormal;
+    public Sprite LTPressed;
+    public Sprite ANormal;
+    public Sprite APressed;
+    public Sprite XNormal;
+    public Sprite XPressed;
+    public Sprite YNormal;
+    public Sprite YPressed;
+
 
     [Header("Show the controls corner?")]
     public bool show = false; 
@@ -69,7 +111,8 @@ public class ControlsCornerUI : MonoBehaviour
         if (initialized && show)
         {
             cornerControlsUI.SetActive(true);
-            updateControlsCornerUI();
+            UpdateControlsCornerUI();
+            UpdateButtonHighlights();
         }
         else if (initialized && !show)
         {
@@ -109,7 +152,7 @@ public class ControlsCornerUI : MonoBehaviour
         Debug.Log("Controls Corner UI successfully initialized");
     }
 
-    public void updateControlsCornerUI()
+    public void UpdateControlsCornerUI()
     {
         // bool isP1Cat = playerManager.P1.Species == "cat";
         bool isP1Front = playerManager.P1.IsFront;
@@ -124,5 +167,42 @@ public class ControlsCornerUI : MonoBehaviour
     public void setShow(bool setting)
     {
         show = setting;
+    }
+
+    public void UpdateButtonHighlights() {
+        InputHandler p1Input = playerManager.player1Input;
+        InputHandler p2Input = playerManager.player2Input;
+
+        if (P1ControlsCF.activeSelf) {
+            p1SwitchIcon.sprite = p1Input.GetSwitchPressed() ? LTPressed : LTNormal;
+            p1ReconnectIcon.sprite = p1Input.GetReconnectPressed() ? RTPressed : RTNormal;
+            p1FrontPawIcon.sprite = p1Input.GetJumpPressed() ? APressed : ANormal;
+            p1MeowIcon.sprite = p1Input.GetSoundTailPressed() ? YPressed : YNormal;
+            p1ClimbIcon.sprite = p1Input.GetSpecialActionPressed() ? XPressed : XNormal;
+        }
+            
+        if (P1ControlsCB.activeSelf) {
+            p1BackSwitchIcon.sprite = p1Input.GetSwitchPressed() ? LTPressed : LTNormal;
+            p1BackReconnectIcon.sprite = p1Input.GetReconnectPressed() ? RTPressed : RTNormal;
+            p1JumpIcon.sprite = p1Input.GetJumpPressed() ? APressed : ANormal;
+            p1TailIcon.sprite = p1Input.GetSoundTailPressed() ? YPressed : YNormal;
+            p1HighJumpIcon.sprite = p1Input.GetSpecialActionPressed() ? XPressed : XNormal;
+        }
+
+        if (P2ControlsDF.activeSelf) {
+            p2SwitchIcon.sprite = p2Input.GetSwitchPressed() ? LTPressed : LTNormal;
+            p2ReconnectIcon.sprite = p2Input.GetReconnectPressed() ? RTPressed : RTNormal;
+            p2FrontPawIcon.sprite = p2Input.GetJumpPressed() ? APressed : ANormal;
+            p2BarkIcon.sprite = p2Input.GetSoundTailPressed() ? YPressed : YNormal;
+            p2GrabIcon.sprite = p2Input.GetSpecialActionPressed() ? XPressed : XNormal;
+        }
+
+        if (P2ControlsDB.activeSelf) {
+            p2BackSwitchIcon.sprite = p2Input.GetSwitchPressed() ? LTPressed : LTNormal;
+            p2BackReconnectIcon.sprite = p2Input.GetReconnectPressed() ? RTPressed : RTNormal;
+            p2JumpIcon.sprite = p2Input.GetJumpPressed() ? APressed : ANormal;
+            p2TailIcon.sprite = p2Input.GetSoundTailPressed() ? YPressed : YNormal;
+            p2DashIcon.sprite = p2Input.GetSpecialActionPressed() ? XPressed : XNormal;
+        }
     }
 }
