df288f15e1f86b992905fe97e2705404401c8c84
Chelsea Wang <chelseawang9@gmail.com>
2025-06-14 17:14:54 -0400

    changed rug trigger from OnTriggerEnter to OnTriggerStay

--- Frankenpets/Assets/Scripts/AtticTasks/AtticDoorTrigger.cs
+++ Frankenpets/Assets/Scripts/AtticTasks/AtticDoorTrigger.cs
@@ -3,15 +3,33 @@ using UnityEngine;
 public class AtticDoorTrigger : MonoBehaviour
 {
     public LevelLoader levelLoader;
+    private float triggerEnterTime = 0.0f;
+    private bool timerStarted = false;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("dog front") || other.CompareTag("cat front"))
+        if (!timerStarted)
         {
-            if (levelLoader != null) 
+            triggerEnterTime = Time.time;
+            timerStarted = true;
+        }
+
+        if (Time.time - triggerEnterTime >= 1.0f && (other.CompareTag("dog front") || other.CompareTag("cat front")))
+        {
+            if (levelLoader != null)
             {
                 levelLoader.LoadNextLevel();
             }
+
+            timerStarted = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("dog front") || other.CompareTag("cat front"))
+        {
+            timerStarted = false;
         }
     }
 }
